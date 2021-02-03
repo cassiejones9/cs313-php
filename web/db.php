2c7f5281be1774ec0ef2517046c8fd52f5fd3598
Cassie Jones <cassie.jones9@gmail.com>
2021-02-02 16:47:15 -0800

    whoknos

--- web/db.php
+++ web/db.php
@@ -29,6 +29,13 @@ $db = get_db();
     }
     return $db;
     }
+
+    foreach ($db->query('SELECT username, password FROM note_user') as $row)
+{
+  echo 'user: ' . $row['username'];
+  echo ' password: ' . $row['password'];
+  echo '<br/>';
+}
     ?>
 </body>
 
