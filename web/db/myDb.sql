e8f0ac55df90277fd391e8fb3e74bf964eb0f0ee
Cassie Jones <cassie.jones9@gmail.com>
2021-02-02 16:43:08 -0800

    whoknows

---
+++ web/db/myDb.sql
@@ -0,0 +1,55 @@
+Drop TABLE if exists client;
+DROP TABLE if exists session;
+
+CREATE TABLE client (
+    clientId SERIAL PRIMARY KEY,
+    username VARCHAR(255)  NOT NULL,
+    pass VARCHAR(255)  NOT NULL,
+    lastName varchar(255)  NOT NULL,
+    firstName varchar(255)  NOT NULL,
+    phone varchar(255)  NOT NULL,
+    city varchar(255)  NOT NULL
+);
+
+INSERT INTO client (username, pass, lastName, firstName, phone, city)
+VALUES ('jonesjohn', 'password', 'Jones', 'John', '123-456-7890', 'Las Vegas');
+
+ALTER TABLE client 
+DROP COLUMN city;
+
+ALTER TABLE client
+ADD COLUMN email VARCHAR;
+
+UPDATE client
+SET email = 'johnjjones@gmail.com'
+WHERE username = 'jonesjohn';
+
+INSERT INTO client (username, pass, lastName, firstName, phone, email)
+VALUES ('smithH', 'pa$$word', 'Smith', 'Hyrum', '012-345-6789', 'North Las Vegas');
+
+INSERT INTO client (username, pass, lastName, firstName, phone, email)
+VALUES ('jdnlv9', 'passWoRd', 'Doe', 'Jane', '901-234-5678', 'North Las Vegas');
+
+INSERT INTO client (username, pass, lastName, firstName, phone, email)
+VALUES ('motherofjoseph', 'passworDDdd', 'Smith', 'Lucy', '345-678-9012', 'Las Vegas');
+
+CREATE TABLE session (
+    sessionId SERIAL PRIMARY KEY,
+    clientId int NOT NULL,
+    sessionDate DATE NOT NULL,
+    numOfPeople smallint NOT NULL,
+    constraint fk_client FOREIGN KEY(clientId) REFERENCES client (clientId)
+);
+
+INSERT INTO session (clientId, sessionDate, numOfPeople)
+VALUES (1, '2021-02-06', 3);
+
+INSERT INTO session (clientId, sessionDate, numOfPeople)
+VALUES (2, '2021-02-10', 8);
+
+INSERT INTO session (clientId, sessionDate, numOfPeople)
+VALUES (3, '2021-03-18', 1);
+
+
+
+
---
+++ web/db/teamassignment.sql
@@ -0,0 +1,12 @@
+Drop TABLE if exists Notes;
+
+CREATE TABLE Notes (
+    NotesID INT NOT NULL IDENTITY PRIMARY KEY,
+    PersonID INT NOT NULL,
+    SpeakerID INT NOT NULL,
+    ConferenceID INT NOT NULL,
+    Note VARCHAR(255) NOT NULL
+);
+
+INSERT INTO Notes(NotesID, PersonID, SpeakerID, ConferenceID, Note)
+VALUES ()
