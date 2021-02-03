c23059f0ef4eb2c5650273c3e7e7a824fae37301
Cassie Jones <cassie.jones9@gmail.com>
2021-02-02 17:06:30 -0800

    h

--- web/dbinfo.php
+++ web/dbinfo.php
@@ -10,12 +10,12 @@ $db = get_db();
 <h2>Database Connection</h2>
 
 <?php 
-foreach ($db->query('SELECT username, password FROM note_user') as $row)
+$statement = $db->query('SELECT username, password FROM note_user');
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
-  echo 'user: ' . $row['username'];
-  echo ' password: ' . $row['password'];
-  echo '<br/>';
+  echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
 }
+
 ?>
 
 </body>
