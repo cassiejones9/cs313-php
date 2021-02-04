5467605f6c0cdca3d59b3106e0ff28f503cabc76
Cassie Jones <cassie.jones9@gmail.com>
2021-02-04 14:23:27 -0800

    5

--- web/project/clientDetails.php
+++ web/project/clientDetails.php
@@ -4,7 +4,7 @@ $db = get_db();
 session_start();
 
 $id = $_SESSION['clientId'];
-$sqlString = 'SELECT * FROM client WHERE id = :id';
+$sqlString = 'SELECT * FROM client WHERE clientId = :clientId';
 $statement = $db->prepare($sqlString);
 $statement->bindValue(':id', $id, PDO::PARAM_INT);
 $statement->execute();
