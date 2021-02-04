b1a677c0626d80474ae40d12a3dfd4bfa994e9cf
Cassie Jones <cassie.jones9@gmail.com>
2021-02-03 16:16:47 -0800

    prove3

---
+++ web/project/clientDetails.php
@@ -0,0 +1,37 @@
+<?php
+include "../db/dbConnect.php";
+$db = get_db();
+session_start();
+
+$id = $_SESSION['clientId'];
+$sqlString = 'SELECT * FROM c.client AS c WHERE id = :id';
+$statement = $db->prepare($sqlString);
+$statement->bindValue(':id', $id, PDO::PARAM_INT);
+$statement->execute();
+$row = $statement->fetch(PDO::FETCH_ASSOC);
+$displayClientInfo .= "$row[firstName] $row[lastName]  Username: $row[username] Password: $row[pass]  Phone: $row[phone]  City: $row[city]"
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>CSE 341 | Photography Client Details</title>
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+  <link rel="stylesheet" href="../css/stylesheet.css" media="screen">
+  <!-- Line 11 fixes favicon error -->
+  <link rel="icon" href="data:;base64,iVBORw0KGgo=">
+</head>
+
+<body>
+  <div class="container">
+    <img src="../images/logo.jpg" class="logo">
+    <h5>Client Data</h5>
+    <?php
+    echo $displayClientInfo;
+    ?>
+  </div>
+</body>
+</html>
--- web/project/dbinfo.php
+++ web/project/dbinfo.php
@@ -3,15 +3,24 @@ include "../db/dbConnect.php";
 $db = get_db();
 session_start();
 
+if (isset($_POST['allcs'])) {
+  $displayClients = "";
+  $statement1 = $db->prepare('SELECT * FROM c.client AS c');
+  $statement1->execute();
+  while ($row = $statement1->fetch(PDO::FETCH_ASSOC)) {
+    $displayClients .= "<a href='clientDetails.php'><h5>$row[firstName] $row[lastName]</h5></a>";
+  }
+}
+
 if (isset($_POST['search'])) {
   $searchClient = $_POST['search'];
-  $sqlString = 'SELECT clientId, username, pass, lastName, firstName, phone, city FROM client AS c WHERE lastName = :searchClient';
+  $sqlString = 'SELECT clientId, username, pass, lastName, firstName, phone, city FROM c.client AS c WHERE lastName = :searchClient';
   $statement = $db->prepare($sqlString);
   $statement->bindValue(':searchClient', $searchClient, PDO::PARAM_STR);
   $statement->execute();
   while($row = $statement->fetch(PDO::FETCH_ASSOC)) {
     $displaySearch .= "$row[firstName] $row[lastName] $row[phone]";
-    $displaySearch .= "<a href=''>Click Here for More Client Info</a>";
+    $displaySearch .= "<a href=''>Click Here for More Client Info on $row[firstname]</a>";
     $_SESSION['clientId'] = $row['clientId'];
   }
 }
@@ -24,7 +33,7 @@ if (isset($_POST['search'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>CSE 341 | Photography Project</title>
+  <title>CSE 341 | Photography Client Database</title>
   <link rel="stylesheet" href="../css/bootstrap.min.css">
   <link rel="stylesheet" href="../css/stylesheet.css" media="screen">
   <!-- Line 11 fixes favicon error -->
@@ -35,12 +44,18 @@ if (isset($_POST['search'])) {
   <div class="container">
     <img src="../images/logo.jpg" class="logo">
     <h2>Database Information</h2>
-    <p>Use the form below to query client data.</p>
-    <h4>See All Clients</h4>
+    
     <form method="POST" action="">
       <label>See All Clients</label>
+      <input type="hidden" name="allcs" value="allclients">
       <input type="submit" name="allclients" value="Go">
     </form>
+    <?php
+      if (isset($displayClients)) {
+        echo $displayClients;
+      }
+    ?>
+    <p>Or use the form below to query clients by last name.</p>
     <form method="POST" action="">
       <label for="search">Enter Client Last Name</label>
       <input type="text" name="search">
