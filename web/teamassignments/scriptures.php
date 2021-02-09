de6bd2800e5e3b186b5e5080ab5606632fb72f3c
Cassie Jones <cassie.jones9@gmail.com>
2021-02-08 16:02:32 -0800

    ta1

---
+++ web/teamassignments/scriptures.php
@@ -0,0 +1,61 @@
+<?php
+require ('connection.php');
+$db = get_db();
+
+
+$display = "<h1>Scripture Resources</h1>";
+$statement = $db->prepare('SELECT id, book, chapter, verse, content FROM scriptures');
+$statement->execute();
+
+while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+  $display .= "<p><strong>Book: $row[book] Chapter: $row[chapter] Verse: $row[verse]</strong>";
+  $display .= " - '$row[content]'</p>";
+}
+
+if (isset($_POST['search'])) {
+  $searchBook = $_POST['search'];
+  $strSql = 'SELECT id, book, chapter, verse, content FROM scriptures WHERE book = :searchBook';
+  $statement = $db->prepare($strSql);
+  $statement->bindValue(':searchBook', $searchBook, PDO::PARAM_STR);
+  $statement->execute();
+
+  //echo $strSql;
+  // exit();
+
+  $displaySearch = "<h1>Scripture Search</h1>";
+
+  while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $displaySearch .= "<a href='detail.php?id=$row[id]'><p><strong>Book: $row[book] Chapter: $row[chapter] Verse: $row[verse]</strong>";
+    $displaySearch .= "'</p></a>";
+
+  }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?
+    echo $display;
+  ?>
+  <form method="POST" action="">
+    <label for="search"></label>
+    <input type="text" name="search">
+    <input type="submit" name="submit" value="Search">
+  </form>
+  <?
+  if (isset($displaySearch)) {
+    echo $displaySearch;
+  }
+  
+  ?>
+</body>
+
+</html>
---
+++ web/teamassignments/ta.sql
@@ -0,0 +1,37 @@
+
+CREATE TABLE scriptures (
+  id SERIAL NOT NULL PRIMARY KEY,
+  book VARCHAR(50) NOT NULL,
+  chapter SMALLINT NOT NULL,
+  verse SMALLINT NOT NULL,
+  content TEXT NOT NULL
+);
+
+INSERT INTO scriptures (book, chapter, verse, content)
+VALUES ('John', 1, 5, 'And the light shineth in darkness; and the darkness comprehended it not.');
+
+INSERT INTO scriptures (book, chapter, verse, content)
+VALUES ('Doctrine and Covenants', 93, 28, 'He that keepeth his commandments receiveth truth and light, until he is glorified in truth and knoweth all things.');
+
+
+INSERT INTO scriptures (book, chapter, verse, content)
+VALUES ('Mosíah', 16, 9, 'He is the light and the life of the world; yea, a light that is endless, that can never be darkened; yea, and also a life which is endless, that there can be no more death.');
+
+
+CREATE TABLE topic (
+  id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(100) NOT NULL
+);
+
+INSERT INTO topic (name) 
+VALUES ('Faith'), 
+('Sacrifice'), 
+('Charity');
+
+CREATE TABLE linking (
+id SERIAL NOT NULL PRIMARY KEY,
+scipture_id INT NOT NULL,
+topic_id INT NOT NULL, 
+FOREIGN KEY (scripture_id) REFERENCES scriptures(id), 
+FOREIGN KEY (topic_id) REFERENCES topic(id)
+);
