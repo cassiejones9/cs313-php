05e5a0d93f27f9dafdd40eb5920c5e2e74373362
Cassie Jones <cassie.jones9@gmail.com>
2021-02-09 20:39:18 -0800

    ta29

--- web/teamassignments/js.js
+++ web/teamassignments/js.js
@@ -1,6 +1,6 @@
 // $("#scriptureform").submit(function (event) {
 //     event.preventDefault(); //prevent default action 
-//     var post_url = $(this).attr("response.php"); //get form action url
+//     var post_url = $(this).attr("insert_scripture.php"); //get form action url
 //     var request_method = $(this).attr("POST"); //get form GET/POST method
 //     var form_data = new FormData(this); //Creates new FormData object
 //     $.ajax({
@@ -11,21 +11,22 @@
 //         cache: false,
 //         processData: false
 //     }).done(function (response) {
-//         console.log(response);
-//         $("#scripturedisplay").html(response);
+//         $("#scripturedisplay").html($display);
 //     });
 // });
 
-
-$.ajax({
-    method: "POST",
-    url: "response.php",
-    data: { name: "Dan", example: "Yup" }
-})
-    .done(function (message) {
-        $("#log").text(message);
+$("#scriptureform").submit(function (event) {
+    event.preventDefault();
+    $.ajax({
+        method: "POST",
+        url: "response.php",
+        data: { name: "Dan", example: "Yup" }
     })
-    .fail(function (message) {
-        $("#log").text(message);
-    });
+        .done(function (message) {
+            $("#log").text(message);
+        })
+        .fail(function (message) {
+            $("#log").text(message);
+        });
 
+});
