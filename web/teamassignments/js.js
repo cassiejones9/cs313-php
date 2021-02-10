4ca95d005046a38d813acca6b15a75544373c3e7
Cassie Jones <cassie.jones9@gmail.com>
2021-02-09 20:26:13 -0800

    ta27

--- web/teamassignments/js.js
+++ web/teamassignments/js.js
@@ -1,6 +1,6 @@
 $("#scriptureform").submit(function(event){
     event.preventDefault(); //prevent default action 
-    var post_url = $(this).attr("insert_scripture.php"); //get form action url
+    var post_url = $(this).attr("response.php"); //get form action url
     var request_method = $(this).attr("POST"); //get form GET/POST method
 	var form_data = new FormData(this); //Creates new FormData object
     $.ajax({
