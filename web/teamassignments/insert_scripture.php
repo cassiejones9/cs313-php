1b8e437efdaddf8e1decf5a5df82911c1da6b86d
Cassie Jones <cassie.jones9@gmail.com>
2021-02-09 17:29:55 -0800

    ta14

--- web/teamassignments/insert_scripture.php
+++ web/teamassignments/insert_scripture.php
@@ -8,9 +8,9 @@ $verse = test_input($_POST['verse']);
 $content = test_input($_POST['content']);
 $topic_ids = $_POST['topic'];
 
-echo $book;
-echo $content;
-echo $topic_ids;
+// echo $book;
+// echo $content;
+// echo $topic_ids;
 
 function test_input($data)
 {
@@ -22,22 +22,23 @@ function test_input($data)
 
 
 // // info just for the scriptures table
-// $stmt = $db->prepare('INSERT INTO scriptures(book, chapter, verse, content) VALUES (:book, :chapter, :verse, :content);');
-// $stmt->bindValue(':book', $book);
-// $stmt->bindValue(':chapter', $chapter);
-// $stmt->bindValue(':verse', $verse);
-// $stmt->bindValue(':content', $content);
-// $stmt->execute();
-// // get the scripture_id from above
-// $scripture_id = $db->lastInsertId("scriptures_id_seq");
-// foreach ($topic_ids as $topic_id){
-//     $stmt = $db->prepare('INSERT INTO linking (scripture_id, topic_id) VALUES (:scripture_id, :topic_id);');
-//     $stmt->bindValue(':scripture_id', $scripture_id);
-//     $stmt->bindValue(':topic_id', $topic_id);
-//     $stmt->execute();
-// }
+$query = 'INSERT INTO scriptures(book, chapter, verse, content) VALUES(:book, :chapter, :verse, :content)';
+$stmt = $db->prepare($query);
+$stmt->bindValue(':book', $book);
+$stmt->bindValue(':chapter', $chapter);
+$stmt->bindValue(':verse', $verse);
+$stmt->bindValue(':content', $content);
+$stmt->execute();
+// get the scripture_id from above
+$scripture_id = $db->lastInsertId("scriptures_id_seq");
+foreach ($topic_ids as $topic_id){
+    $stmt = $db->prepare('INSERT INTO linking(scripture_id, topic_id) VALUES(:scripture_id, :topic_id);');
+    $stmt->bindValue(':scripture_id', $scripture_id);
+    $stmt->bindValue(':topic_id', $topic_id);
+    $stmt->execute();
+}
 
-// header("Location: showScriptures.php");
+header("Location: showScriptures.php");
 
 // if(isset($stmt)){
 //     echo "<h2>Your scripture was added to the database.</h2>";
