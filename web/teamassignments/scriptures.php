75d2ef0d9f21afa97a1e6ae9ad7fa9ede675888a
Cassie Jones <cassie.jones9@gmail.com>
2021-02-09 18:25:49 -0800

    ta16

--- web/teamassignments/scriptures.php
+++ web/teamassignments/scriptures.php
@@ -71,14 +71,14 @@ if (isset($_POST['search'])) {
     $topics = $stmt->fetchAll(PDO::FETCH_ASSOC);
     $topicarray = array();
     foreach ($topics as $topic) {
-      $topicid = $topic['id'];
-      $topicname = $topic['name'];
+      $topicid = $topic["id"];
+      $topicname = $topic["name"];
     
       echo "<input type='checkbox' name='topic[]' value='$topicid'>";
       echo "<label for='topic'>$topicname</label><br>";
       
     }
-    // if (isset($_POST['topic'])) {
+    // if (isset($_POST["topic"])) {
 
     // }
     ?>
