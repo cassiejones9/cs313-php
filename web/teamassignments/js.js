b0177a31a260b779b70ea0d32ab4adb4e08fe211
Cassie Jones <cassie.jones9@gmail.com>
2021-02-09 20:44:57 -0800

    ta30

--- web/teamassignments/js.js
+++ web/teamassignments/js.js
@@ -1,32 +1,33 @@
-// $("#scriptureform").submit(function (event) {
-//     event.preventDefault(); //prevent default action 
-//     var post_url = $(this).attr("insert_scripture.php"); //get form action url
-//     var request_method = $(this).attr("POST"); //get form GET/POST method
-//     var form_data = new FormData(this); //Creates new FormData object
-//     $.ajax({
-//         url: post_url,
-//         type: request_method,
-//         data: form_data,
-//         contentType: false,
-//         cache: false,
-//         processData: false
-//     }).done(function (response) {
-//         $("#scripturedisplay").html($display);
-//     });
-// });
-
 $("#scriptureform").submit(function (event) {
-    event.preventDefault();
+    event.preventDefault(); //prevent default action 
+    var post_url = $(this).attr("insert_scripture.php"); //get form action url
+    var request_method = $(this).attr("POST"); //get form GET/POST method
+    var form_data = new FormData(this); //Creates new FormData object
     $.ajax({
-        method: "POST",
-        url: "response.php",
-        data: { name: "Dan", example: "Yup" }
-    })
-        .done(function (message) {
-            $("#log").text(message);
-        })
-        .fail(function (message) {
-            $("#log").text(message);
-        });
-
+        url: post_url,
+        type: request_method,
+        data: form_data,
+        contentType: false,
+        cache: false,
+        processData: false
+    }).done(function (response) {
+        $("#scripturedisplay").html($display);
+    });
 });
+
+// $("#scriptureform").submit(function (event) {
+//     event.preventDefault();
+//     $.ajax({
+//         method: "POST",
+//         url: "response.php",
+//         data: { name: "Dan", example: "Yup" }
+//     })
+//         .done(function (message) {
+//             $("#log").text(message);
+//         })
+//         .fail(function (message) {
+//             $("#log").text(message);
+//             console.log(message);
+//         });
+
+// });
